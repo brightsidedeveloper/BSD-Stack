acfb149c5d3d50468f26eb8d86fc5e9f461b9877
tim vanlerberg <tim@brightsidedeveloper.com>
2024-12-25 12:12:44 -0600

    Thats me lol

---
+++ server/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib" // PostgreSQL driver
+)
+
+var DB *sql.DB
+
+func Connect() {
+	var err error
+
+	// Update the connection string if necessary
+	dsn := "postgres://postgres:postgres@localhost:5432/myappdb?sslmode=disable"
+
+	DB, err = sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database: %v\n", err)
+	}
+
+	// Test the connection
+	if err := DB.PingContext(context.Background()); err != nil {
+		log.Fatalf("Error pinging database: %v\n", err)
+	}
+
+	log.Println("Database connected successfully!")
+}
--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"go-pmp/db"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,12 +14,15 @@ type Test struct {
 }
 
 func main() {
+
+	db.Connect()
+
 	r := chi.NewRouter()
 	r.Get("/api/health", func(w http.ResponseWriter, r *http.Request) {
 
 		test := Test{
-			Name: "John",
-			Age:  25,
+			Name: "Tim",
+			Age:  23,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
